4d156ce14697e6f8879c386a4faab9d1462e8dc3
DefNotAvg <DefNotAvg@gmail.com>
2018-05-06 08:57:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Nike-Order-Checker
+
+Easier way to check multiple Nike.com orders.
+
+*Requires Python 2.7 and Requests
+
+### Setup
+
+1. Store orders in "orders.txt" in email:ordernumber format with one order per line
+2. Store proxies in "proxies.txt" in ip:host or ip:host:user:pass format with one proxy per line
+3. If not using proxies and checking a large number of orders, feel free to edit the DELAY in main.py
+4. Run main.py
+
+### TODO
+
+- [ ] Idk yet, suggest some things on Twitter @DefNotAvg
---
+++ main.py
@@ -0,0 +1,127 @@
+import requests
+import json
+import os
+import random
+from time import sleep
+
+RESULTS = 'orders.txt' # Text file where orders are located in email:ordernumber format
+DELAY = 0 # Delay in seconds between checking orders
+PROXY_FILE = 'proxies.txt' # Text file where proxies are located in ip:host or ip:host:user:pass format
+
+WIDTH = 30 # Don't touch
+
+def center(string, spacer):
+	count = ((WIDTH - len(string)) / 2)
+	if count > 0:
+		return (count - 1) * spacer + ' ' + string + ' ' + (count - 1) * spacer
+	else:
+		return string
+
+def header():
+	if os.name == 'nt':
+		os.system('cls')
+	else:
+		os.system('clear')
+	print center('Nike Order Checker', '~')
+	print center('Made by @DefNotAvg', '~')
+	print ''
+
+def smart_sleep(DELAY):
+	for a in xrange (DELAY, 0, -1):
+		print 'Sleeping for {} seconds...\r'.format(str(a)),
+		sleep(1)
+	print 'Sleeping for {} seconds complete!'.format(str(DELAY))
+
+def set_proxy(PROXIES):
+	if PROXIES != []:
+		proxy = random.choice(PROXIES)
+		proxies = {
+		  'http': 'http://{}'.format(proxy),
+		  'https': 'http://{}'.format(proxy),
+		}
+	else:
+		proxies = {
+		  'http': None,
+		  'https': None,
+		}
+	return proxies
+
+header()
+with open(RESULTS, 'r') as myfile:
+	text = myfile.read()
+info = [item for item in text.split('\n') if item != '']
+statuses = []
+if len(info) > 0:
+	print center('Gathering order status for {} orders...\n'.format(str(len(info))), ' ')
+else:
+	print center('No orders found in {}'.format(RESULTS), ' ')
+sleep(1)
+try:
+	with open(PROXY_FILE, 'r') as myfile:
+		PROXIES = ['{}:{}@{}:{}'.format(PROXY.split(':')[2], PROXY.split(':')[3], PROXY.split(':')[0], PROXY.split(':')[1]) if PROXY != '' and PROXY.count(':') == 3 else PROXY for PROXY in myfile.read().split('\n') if PROXY != '']
+except:
+	with open(PROXY_FILE, 'w') as myfile:
+		PROXIES = myfile.write('')
+	PROXIES = []
+if PROXIES == []:
+	proxies = set_proxy(PROXIES)
+for i in range(0,len(info)):
+	header()
+	print '[{}/{}] Gathering order status...'.format(str(i + 1), str(len(info)))
+	line = info[i]
+	if PROXIES != []:
+		proxies = set_proxy(PROXIES)
+	session = requests.Session()
+	order = line.split(':')
+	email = order[0]
+	order_number = order[1]
+	headers = {
+	    'origin': 'https://secure-store.nike.com',
+	    'accept-encoding': 'gzip, deflate, br',
+	    'accept-language': 'en-US,en;q=0.9',
+	    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    'accept': '*/*',
+	    'referer': 'https://secure-store.nike.com/common/content/endpoint.html',
+	    'authority': 'secure-store.nike.com',
+	    'x-requested-with': 'XMLHttpRequest',
+	}
+	data = [
+	  ('action', 'getAnonymousOrderDetails'),
+	  ('lang_locale', 'en_US'),
+	  ('country', 'US'),
+	  ('endpoint', 'getAnonymousOrderDetails'),
+	  ('orderId', order_number),
+	  ('email', email),
+	  ('deviceType', 'desktop'),
+	]
+	response = session.post('https://secure-store.nike.com/nikestore/html/services/orders/orderDetails', headers=headers, data=data, proxies=proxies)
+	try:
+		status = json.loads(response.content)['data']['order']['status']
+	except:
+		status = 'Error getting order status'
+	statuses.append(status)
+	if len(order) > 2:
+		order[2] = status
+		final = ':'.join(order[:3])
+	else:
+		order.append(status)
+		final = ':'.join(order[:3])
+	with open(RESULTS, 'r') as myfile:
+		text = myfile.read()
+	with open(RESULTS, 'w') as myfile:
+		myfile.write(text.replace(line, final))
+	if DELAY > 0 and i != len(info) - 1:
+		print 'Order {}: {}\n'.format(order_number, status)
+		smart_sleep(DELAY)
+if len(info) > 0:
+	header()
+	print center('Output', '~')
+	print ''
+	for category in list(set(statuses)):
+		print center('{}: {}'.format(category, str(sum(category == status for status in statuses))), ' ')
+	raw_input('\n' + WIDTH * '~' + '\n\n' + center('Output saved to {}'.format(RESULTS), ' ') + '\n\n' + center('Press enter to quit', ' '))
+else:
+	raw_input('\n' + center('Press enter to quit', ' '))
+
+quit()
